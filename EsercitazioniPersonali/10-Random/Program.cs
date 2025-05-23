a9446ff0f4276792eb4eb30d1e3a2b4e122952f6
FrancescoM1993 <francescomarzano93@gmail.com>
2025-05-23 18:00:06 +0200

    Aggiunto 10 Random

--- EsercitazioniPersonali/10-Random/Program.cs
+++ EsercitazioniPersonali/10-Random/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Random numeroRandom = new Random();
+for (int i = 0; i < 10; i++)
+{
+    int numero = numeroRandom.Next(1, 101); // genero un numero casuale tra 1 e 100
+    if (numero % 3 == 0 || numero % 5 == 0)
+    {
+        Console.WriteLine(numero);
+    }
+}
+
+List<int> numeriPari = new List<int>();
+Random numeroRandom = new Random();
+for (int i = 0; i < 10; i++)
+{
+    int numero = numeroRandom.Next(1, 101);
+    if (numero % 2 == 0)
+    {
+        numeroPari.Add(numero);
+    }
+}
+ConsoleWriteline("numeri pari:");
+foreach (int numero in numeriPari)
+{
+    Console.WriteLine(numero);
+}
+
+*/
+Random numeroRandom = new Random();
+Console.WriteLine("Tira il primo dado");
+int dado1 = numeroRandom.Next(1, 10);
+Console.WriteLine("Tira il secondo dado");
+int dado2 = numeroRandom.Next(1, 10);
+int prodotto = dado1 + dado2;
+
+for ()
